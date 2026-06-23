11123f4cfa16d701bd094239ba9ccfd186091ab0
Daffa <daffaseta16@gmail.com>
2026-06-23 10:11:35 +0800

    latihan.js

--- latihan.js
+++ latihan.js
@@ -1,1 +1,53 @@
 console.log("Hello guys")
+var nama = "jhon Doe"
+var nomorUrut = 12
+console.log("nama")
+console.log(nama)
+console.log(nomorUrut)
+var hari = "Jum'at"
+console.log(hari)
+var quote = '"halo apakabar"'
+console.log(quote)
+var open = true
+if(open == true){
+    console.log("BUKA")
+}
+
+// Operator Aritmatika
+var a = 5
+var b = 5
+var tambah = a + b
+var kurang = a - b
+var kali = a * b
+var bagi = a / b
+var modulus = a % b
+console.log(a)
+console.log(b)
+console.log('hasil tambah :', tambah)
+console.log('hasil kurang :', kurang)
+console.log('hasil kali :', kali)
+console.log('hasil bagi :', bagi)
+console.log('hasil modulus :', modulus)
+
+// Operator Perbandingan
+console.log("Operator Perbandingan")
+console.log("=======================")
+console.log(1==1)
+console.log("1"==="1")
+console.log(1==2)
+console.log(11 != 11)
+console.log(3<1)
+console.log(3<3)
+console.log(5>=6)
+console.log(5<=6)
+
+// Operator Kodisional
+console.log("Operator Kodisional")
+console.log("=====================")
+
+// &&(AND) || (OR)
+//console.log(false || false)
+var status = "close"
+var jamBuka = 9
+
+console.log(status == "open" && jamBuka == 9)

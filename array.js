569f82ae81ff71a2b94be24560965f4b9fb8a25e
Daffa <daffaseta16@gmail.com>
2026-06-24 14:17:11 +0800

    array

---
+++ array.js
@@ -0,0 +1,27 @@
+var angka = [2, 3, 1, 6, 8]
+console.log(angka[0])
+console.log(angka[3])
+console.log(angka)
+
+// length
+console.log(angka.length)
+
+// push
+console.log("sebelum push : "+angka)
+angka.push(9)
+console.log("setelah push : "+angka)
+
+// pop
+console.log("sebelum pop : " + angka)
+angka.pop()
+console.log("setelah pop : " + angka)
+
+// unshift
+console.log("sebelum unshift : "+angka)
+angka.unshift(8)
+console.log("setelah unshift : "+angka)
+
+// shift
+console.log("sebelum shift : "+angka)
+angka.shift()
+console.log("setelah shift : "+angka)

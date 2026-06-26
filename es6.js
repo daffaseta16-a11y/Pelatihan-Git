136a21f55a9edf417aeeab5e01d6258c90932f9a
Daffa <daffaseta16@gmail.com>
2026-06-26 14:28:14 +0800

    es6

--- es6.js
+++ es6.js
@@ -45,8 +45,156 @@
 // let nama = "Na Jaemin"
 // console.log(`halo!, ${nama}. selamat datang!`)
 
-text1 = "halo"
-text2 = "selamat"
-text3 = "datang"
+// text1 = "halo"
+// text2 = "selamat"
+// text3 = "datang"
 
-console.log(`${text1} ${text2} ${text3}`)
+// console.log(`${text1} ${text2} ${text3}`)
+
+// Enhanced object Literal
+// let nama = "jaemin"
+// let person = {
+//     nama : nama,
+//     umur : 28
+// }
+
+// console.log(person)
+
+//Destructuring Array
+// let number = [1,3,8,6,7]
+
+// let number1 = number[0]
+// let number2 = number[1]
+// let number3 = number[2]
+// let number4 = number[3]
+// let number5 = number[4]
+
+// let [num1, num2, num3, num4, lastNum] = [1,3,8,6,7]
+
+// console.log(num3)
+
+//Destrucring Object
+// let nama = "jaemin"
+// let person = {
+//     nama : nama,
+//     umur : 28,
+//     tinggi : 173
+// }
+
+// // let namaPerson = person.nama
+// // let umurPerson = person.umur
+// // let tinggiPerson = person.tinggi
+// // let {nama, umur, tinggi} = person
+// console.log(tinggi)
+
+//Rest Parameter
+// let [num1, , , , lastNum] = [1,3,8,6,7]
+
+// let footballers = ["messi", "ronaldo", "mbapee", "halland", "neymar"]
+// // let [foot1, foot2, foot3, foot4, foot5] = footballers
+// let [foot1,foot2,...restFoots] = footballers
+
+// console.log(restFoots)
+
+// let names = "jaemin"
+// let person = {
+//     nama : names,
+//     umur : 26,
+//     tinggi : 178
+// }
+
+// let {nama, ...restPerson} = person
+// console.log(nama)
+// console.log(restPerson)
+
+// Spread Operatror
+// let buah = ["strawberry", "banana", "watermelon"]
+// // buah.unshift("manggo")
+// // buah.push("peach")
+// // console.log(buah)
+
+// buah = ["Manggo", ...buah,"jeruk"]
+// console.log(buah)
+
+// let botol = {
+//     merk : "Crystalin",
+//     ukuran : "1 liter",
+//     harga : 10000
+// }
+
+//  botol = {...botol, warna: "biru", kemasan: "kaca",bentuk : "bulat"}
+// console.log(botol)
+
+// var isMomHappy = false
+
+// // Promise
+// var willIGetNewPhone = new Promise(
+//     function (resolve, reject) {
+//         if (isMomHappy){
+//             var phone = {
+//                 brand: 'samsung',
+//                 color: 'black'
+//             };
+//             resolve(phone);
+//         } else {
+//             var reason = new Error('mom is not happy');
+//             reject(reason);
+//         }
+    
+//     }
+// );
+
+// function askMom() {
+//     willIGetNewPhone
+//     .then(function (terpenuhi) {
+//         console.log(terpenuhi);
+//     })
+//     .catch(function (gagal) {
+//         console.log(gagal.message);
+//     });
+// }
+
+// askMom()
+
+// function perikasaDataPasien(nomorIdPasien) {
+//     var dataPasien = [
+//         {id: 1, nama: "jhon", jenisKelamin: "laki-laki"}
+//         {id: 2, nama: "boo", jenisKelamin: "laki-laki"}
+//         {id: 3, nama: "grace", jenisKelamin: "perempuan"}
+//         {id: 4, nama: "jaemin", jenisKelamin: "laki-laki"}
+//     ]
+//     return new Promise( function(resolve, reject){
+//         var pasien == dataPasien.find(x=> x.id === nomorIdPasien)
+//         if (pasien === undefined){
+//             reject("data pasien tidak ada")
+//         }else{
+//             resolve(pasien)
+//         }
+//     })
+// }
+
+// periksaDataPasien(5).then(function(data){
+//     console.log(data)
+// }).catch(function(err){
+//     console.log(err)
+// })
+
+// Async/await
+
+ function doAsync() {
+    return new Promise( function (resolve, reject){
+        var check = true
+        if (check){
+            resolve("berhasil")
+        }else{
+            reject("gagal")
+        }
+    })
+}
+
+async function hello() {
+    var result = await doAsync()
+    console.log(result)
+}
+
+hello()

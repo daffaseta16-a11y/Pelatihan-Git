783f1510484c500c2861be5a9dd9bffa14122ddc
Daffa <daffaseta16@gmail.com>
2026-06-29 11:01:50 +0800

    oop

--- oop.js
+++ oop.js
@@ -33,21 +33,67 @@
 
 // mycar = new Car("Ford")
 // console.log(mycar.present("hello"))
-// mycar = new Car("Toyota")
-// console.log(mycar.present("hello"))
+// mycar2 = new Car("Toyota")
+// console.log(mycar2.present("hello"))
 
 //Cara lain 1
-class Car {
-    constructor(brand, mobilku) {
-        this.carname = brand;
-        this.mobilku = mobilku
+// class Car {
+//     constructor(brand, mobilku) {
+//         this.carname = brand;
+//         this.mobilku = mobilku
+//     }
+//     present(x) {
+//         return x + ", i have a " + this.carname;
+//     }
+// }
+
+// mycar = new Car("Ford")
+// console.log(mycar.present("hello"))
+// mobilku = new Car("Toyota")
+// console.log(mobilku.present("hei"))
+
+//setter & getter
+// class Car {
+//     constructor(brand) {
+//         this._carname = brand;
+//     }
+//     get carname() {
+//         return this._carname;
+//     }
+//     set carname(x) {
+//         this._carname = x;
+//     }
+// }
+
+// mycar = new Car("BMW");
+// mycar.carname = "kijang"
+
+// inheritance
+class Person{
+    constructor(nama){
+        this.nama = nama
+        this._mapel = ""
     }
-    present(x) {
-        return x + ", i have a " + this.carname;
+    get personMapel(){
+        return this._mapel
+    }
+    set personMapel(mapel){
+        this._mapel = mapel
+    }
+
+    berjalan(){
+        return "ui ui ui"
     }
 }
 
-mycar = new Car("Ford")
-console.log(mycar.present("hello"))
-mobilku = new Car("Toyota")
-console.log(mobilku.present("hei"))
+class School extends Person{
+    constructor(nama){
+        super(nama)
+        this.School = ""
+    }
+}
+
+var guru = new School("Pak Jaemin")
+guru.personMapel = "matematika"
+console.log(guru)
+console.log(guru.berjalan())

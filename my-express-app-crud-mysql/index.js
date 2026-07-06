e9175123d04eb2a438d67122322ded6f471415c0
Daffa <daffaseta16@gmail.com>
2026-07-06 16:31:38 +0800

    userRouter

--- my-express-app-crud-mysql/index.js
+++ my-express-app-crud-mysql/index.js
@@ -1,18 +1,25 @@
+require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const { moviesRouter } = require('./src/route/moviesRouter');
 // const { moviesRouter } = require('./src/route/moviesRouter');
 const app = express();
-const port = 3000;
+const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('helo jaem!');
-// });
+var corsOptions = {
+origin: 'http://localhost:3000',
+optionsSuccessStatus: 200
+}
 
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api', moviesRouter);
 //app.use("/movies/:id", )
 
+// app.get('/', (req, res) => {
+//   res.send('helo jaem!');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
